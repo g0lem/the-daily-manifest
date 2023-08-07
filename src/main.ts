8fc7eb2b76899f61ade12d7f93262b39dee3e298
g0lem <sasteve999@gmail.com>
2023-08-07 15:59:14 +0300

    Added a basic frame counter

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,23 @@ const canvas : HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('a
 
 const context : CanvasRenderingContext2D = canvas.getContext('2d')!;
 
-context.beginPath();
-context.moveTo(0,0);
-context.lineTo(300,300);
-context.stroke();
+let counter : number = 0;
+let timer : number = Date.now();
+
+const drawLine : ()=>void = () => {
+  if(Date.now() - timer > 1000) {
+    console.log(counter);
+    timer = Date.now();
+    counter = 0;
+  }
+  context.clearRect(0,0,300,300);
+  context.beginPath();
+  context.moveTo(0,0);
+  context.lineTo(300,300);
+  context.stroke();
+  counter++;
+}
+
+setInterval(()=> {
+  drawLine();
+},0)
